a7271a94a63651a55a4202f48a3e298f84491c08
zizimoos_iMac <zizimoos@gmail.com>
2021-03-01 17:31:45 +0900

    10.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from . import models
+
+# from django.http import HttpResponse
+
 
 # Create your views here.
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(
+        request,
+        "rooms/all_rooms.html",
+        context={
+            "rooms": all_rooms,
+        },
+    )
