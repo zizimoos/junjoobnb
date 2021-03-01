298aea65a7ed0dabf307ab02a5157473457bec28
zizimoos_iMac <zizimoos@gmail.com>
2021-03-01 23:23:00 +0900

    11.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 # from django.http import HttpResponse
@@ -7,20 +8,25 @@ from . import models
 
 # Create your views here.
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = models.Room.objects.count() / page_size
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    # page = int(page or 1)
+    # page_size = 10
+    # limit = page_size * page
+    # offset = limit - page_size
+    # all_rooms = models.Room.objects.all()[offset:limit]
+    # page_count = models.Room.objects.count() / page_size
+
     return render(
         request,
         "rooms/all_rooms.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": ceil(page_count),
-            "page_range": range(1, ceil(page_count)),
-        },
+        # context={
+        #     "rooms": all_rooms,
+        #     "page": page,
+        #     "page_count": ceil(page_count),
+        #     "page_range": range(1, ceil(page_count)),
+        # },
+        {"rooms": rooms},
     )
