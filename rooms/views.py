b07de284b791a97230b43d0e3203e9a2867a8f72
zizimoos_iMac <zizimoos@gmail.com>
2021-03-02 23:52:45 +0900

    11.8

--- rooms/views.py
+++ rooms/views.py
@@ -1,32 +1,39 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
+
 # from django.http import HttpResponse
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
 
 
 # Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    # page = int(page or 1)
-    # page_size = 10
-    # limit = page_size * page
-    # offset = limit - page_size
-    # all_rooms = models.Room.objects.all()[offset:limit]
-    # page_count = models.Room.objects.count() / page_size
-
-    return render(
-        request,
-        "rooms/all_rooms.html",
-        # context={
-        #     "rooms": all_rooms,
-        #     "page": page,
-        #     "page_count": ceil(page_count),
-        #     "page_range": range(1, ceil(page_count)),
-        # },
-        {"rooms": rooms},
-    )
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10, orphans=3)
+#     try:
+#         rooms = paginator.get_page(int(page))
+#         return render(
+#             request,
+#             "rooms/all_rooms.html",
+#             {"page": rooms},
+#         )
+#     except EmptyPage:
+#         return redirect("/")
