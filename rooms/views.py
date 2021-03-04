626bbb3b0e22e6c36252892005cdac3b265d5a19
zizimoos_iMac <zizimoos@gmail.com>
2021-03-04 23:35:04 +0900

    12.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
-from django.views.generic import ListView
-from django.urls import reverse
+from django.views.generic import ListView, DetailView
+
+# from django.http import Http404
 from django.utils import timezone
-from django.shortcuts import render, redirect
+
+# from django.shortcuts import render
 from . import models
 
 
@@ -25,9 +27,17 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         # return redirect(reverse("core:home"))
+#         raise Http404()
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
