b1b006e354347c6f56a2a3b93dfbc004ce334143
zizimoos_iMac <zizimoos@gmail.com>
2021-03-14 01:20:11 +0900

    20.12

--- users/views.py
+++ users/views.py
@@ -34,11 +34,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "black",
-        "last_name": "pink",
-        "email": "blackpink@gmail.com",
-    }
 
     def form_valid(self, form):
         form.save()
